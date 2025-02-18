e3ac27caa11cbbf8aa9de0169d0fa4150eca40bc
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-18 15:29:24 +0100

    add turn on lamp functionality

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const lamp = document.querySelector('.lamp');
 const button = document.querySelector('.btn');
 
 // event listener on btn
+button.addEventListener('click', function() {
+    lamp.src = "./img/yellow_lamp.png";
+})
 
 // img and btn change on click
 
