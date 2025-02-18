b54c524823cd7da8622dd3a1231031d7a9f62e8b
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-02-18 15:44:57 +0100

    add bg change on click

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const lamp = document.querySelector('.lamp');
 const button = document.querySelector('.btn');
+const background = document.querySelector('.main-bg');
 
 // event listener on btn
 button.addEventListener('click', function() {
@@ -8,9 +9,13 @@ button.addEventListener('click', function() {
     if(button.innerText.toLowerCase() == 'accendi'){
         lamp.src = "./img/yellow_lamp.png"; 
         button.innerText = 'Spegni';
+        background.classList.remove('bg-dark');
+        background.classList += " bg-light";
     } else {
         button.innerText = 'Accendi';
         lamp.src = "./img/white_lamp.png";
+        background.classList.remove('bg-light');
+        background.classList += " bg-dark";
     }
 })
 
